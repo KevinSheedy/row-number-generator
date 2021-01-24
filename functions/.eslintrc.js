bf8ce0dea6c8887523e52d1d83a7d0d02247ba29
Kevin Sheedy <kevinsheedy@gmail.com>
2021-01-24 13:06:04 +0000

    eslint config

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,13 +1,11 @@
 module.exports = {
   root: true,
+  parser: "babel-eslint",
   env: {
     es6: true,
     node: true,
   },
-  extends: [
-    "eslint:recommended",
-    "google",
-  ],
+  extends: ["eslint:recommended", "google", "prettier"],
   rules: {
     quotes: ["error", "double"],
   },
